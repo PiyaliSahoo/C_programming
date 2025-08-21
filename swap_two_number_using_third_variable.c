f378b2b2120f97549dd6860b3182c3915c8d1434
PiyaliSahoo <sahoopiyali16@gmail.com>
2025-08-21 23:42:44 +0530

    swap two number using third variable

---
+++ swap_two_number_using_third_variable.c
@@ -0,0 +1,11 @@
+    #include <stdio.h>
+
+    int main() 
+    {
+        int a=10,b=15,c;
+        c=a;
+        a=b;
+        b=c;
+        printf("The swaped numbers:a=%d,b=%d",a,b);
+        return 0;
+    }
