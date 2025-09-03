95500e391e47db603c882be4ab18d47d59f3ae13
PiyaliSahoo <sahoopiyali16@gmail.com>
2025-09-03 09:47:16 +0530

    Find out the floor and ceiling value of a positive and negative number

---
+++ Find_out_the_floor_and_ceiling_value_positive_and_negative_number.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include<math.h>
+
+int main() 
+{
+    float a,b;
+    printf("Enter positive num value:");
+    scanf("%f", &a);
+    printf("Enter negative num value:");
+    scanf("%f", &b);
+    printf("\nFloor value for positive num a:%.2f",floor(a));
+    printf("\nCeil value for positive num a:%.2f",ceil(a));
+    printf("\nFloor value for negative num b:%.2f",floor(b));
+    printf("\nCeil value for negative num b:%.2f",ceil(b));
+    return 0;
+}
