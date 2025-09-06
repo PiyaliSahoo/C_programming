6094a0bfc84f9f8149996e14675531395d5c972c
PiyaliSahoo <sahoopiyali16@gmail.com>
2025-09-06 20:13:32 +0530

    Check whether roll 100 is present or not using if only

---
+++ Check_whether_roll_100_is_present_or_not_using_if_only.c
@@ -0,0 +1,13 @@
+    #include <stdio.h>
+
+    int main() 
+    {
+        int a;
+        printf("Enter rollnumber:");
+        scanf("%d", &a);
+        if(a==100)
+        {
+	 printf("Roll 100 is present");
+	}
+        return 0;
+    }
