77eda931d17f9d52844aef2b9012bbfac94b1c1b
PiyaliSahoo <sahoopiyali16@gmail.com>
2025-09-09 20:00:39 +0530

    Check whether it is leap year or not using ladder- if -else

---
+++ Check_whether_it_is_leap_year_or_not_using_ladder_if_else.c
@@ -0,0 +1,17 @@
+    #include <stdio.h>
+
+    int main() 
+    {
+        int a;
+        printf("Enter a year:");
+        scanf("%d", &a);
+        if(a%4==0)
+        {
+	 printf("%d is a leap year.\n",a);
+	}
+        else
+        {
+         printf("%d is not a leap year.\n",a);
+        }
+        return 0;
+    }
