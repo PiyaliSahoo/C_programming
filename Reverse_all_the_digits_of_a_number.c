e99b8c696d5e8a6eb534b736a4c6c9600cc60ad2
PiyaliSahoo <sahoopiyali16@gmail.com>
2025-11-05 00:46:41 +0530

    Reverse all the digits of a number

---
+++ Reverse_all_the_digits_of_a_number.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() 
+{
+    int n, r_n = 0, r;
+    printf("Enter an integer:");
+    scanf("%d", &n);
+    while(n != 0){
+     r = n % 10;
+     r_n = r_n * 10 + r;
+     n /= 10;
+    }
+    printf("Reverse Number = %d\n", r_n);
+    return 0;
+  }
+  
